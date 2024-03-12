517ab696a729c208b2aed49aba9d4501456ebf31
Ritesh Patel <ritesh.patel@adhocteam.us>
2024-03-12 08:59:40 -0500

    fix: remove comments

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function hello() {
     return "hello from hellow-npm!"
   }
   //adding comment for the fix
-  //add more comments and more and more comments
+  //add more comments
   
   module.exports = hello
