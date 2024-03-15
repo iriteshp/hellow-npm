12960219550a9b2c5f2a0e5c12825fb48e8ab3cd
Ritesh Patel <ritesh.patel@adhocteam.us>
2024-03-15 10:52:54 -0500

    update packages

--- bump-version.js
+++ bump-version.js
@@ -1,20 +1,25 @@
 const { writeFileSync } = require('fs');
 
-// Read the current version of Package 1
-const package1 = require('./packages/package1/package.json');
-const currentVersion = package1.version;
+var args = process.argv.slice(2);
 
-// Update the dependencies in Package 2 and Package 3
-const package2 = require('./packages/package2/package.json');
-const package3 = require('./packages/package3/package.json');
+//check to see if script ran from package 1.
+if (args[0].includes("package1")) {
+    const package1 = require('./packages/package1/package.json');
+    const currentVersion = package1.version;
+    console.log("found current version for the package 1:", currentVersion)
 
-// package1.dependencies['rp-hellow-package3'] = `^${currentVersion}`;
-package2.dependencies['rp-hellow-package1'] = `^${currentVersion}`;
-package3.dependencies['rp-hellow-package1'] = `^${currentVersion}`;
+    // Update the dependencies in Package 2 and Package 3
+    const package2 = require('rp-hellow-package2/package.json');
+    const package3 = require('rp-hellow-package3/package.json');
 
-// Write the updated package.json files
-// writeFileSync('./packages/package1/package.json', JSON.stringify(package1, null, 2));
-writeFileSync('./packages/package2/package.json', JSON.stringify(package2, null, 2));
-writeFileSync('./packages/package3/package.json', JSON.stringify(package3, null, 2));
+    package2.dependencies['rp-hellow-package1'] = `^${currentVersion}`;
+    package3.dependencies['rp-hellow-package1'] = `^${currentVersion}`;
 
-console.log('Updated dependencies in packages');
+    writeFileSync('../package2/package.json', JSON.stringify(package2, null, 2));
+    writeFileSync('../package3/package.json', JSON.stringify(package3, null, 2));
+
+    console.log('Updated dependencies in packages');
+}
+else {
+    console.log('No dependencies to update')
+}
