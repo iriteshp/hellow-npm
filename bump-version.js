ec91011c0db92b57ee7e14a7cb7f05a8e91373fc
Ritesh Patel <ritesh.patel@adhocteam.us>
2024-03-15 11:55:48 -0500

    feat: adding new feature release

--- bump-version.js
+++ bump-version.js
@@ -1,8 +1,8 @@
 const { writeFileSync } = require('fs');
 
 var args = process.argv.slice(2);
-console.log(args)
-//check to see if script ran from package 1.
+
+//Check to see if script ran from package 1.
 if (args[0].includes("package1")) {
     const package1 = require('./packages/package1/package.json');
     const currentVersion = package1.version;
@@ -15,6 +15,7 @@ if (args[0].includes("package1")) {
     package2.dependencies['rp-hellow-package1'] = `^${currentVersion}`;
     package3.dependencies['rp-hellow-package1'] = `^${currentVersion}`;
 
+    // Write back package.json
     writeFileSync('../package2/package.json', JSON.stringify(package2, null, 2));
     writeFileSync('../package3/package.json', JSON.stringify(package3, null, 2));
 
